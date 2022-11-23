ef6043ef8988bbc2aaee111168b6fd929208906f
daniel155158 <daniel155158@gmail.com>
2022-11-23 13:38:39 +0800

    feat: add home page

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const port = 3000
 
 //Template engine setting
-app.engine('hbs', exphbs({ defailtLayout: 'main', extname: 'hbs' }))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 
+//App.use setting
+app.use(bodyParser.urlencoded())
+
+//Route setting
 app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.post('/', (req, res) => {
   res.render('home')
 })
 
